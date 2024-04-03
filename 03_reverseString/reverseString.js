85d8df6d2f8b5496a58c2b31ec7df19e933d5b3c
moises0142 <lopez.moises0142@gmail.com>
2024-04-03 16:03:51 -0400

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(word) {
+    let i= 0;
+    let wLength = word.length;
+    let lettersSeparated="";
+    let Reversed="";
+    while( i <= wLength ){
+        lettersSeparated += [word.charAt(i)];
+        i++;
+    }
+    let b =1;
+    while( b<=(wLength) ){
+        letters = lettersSeparated[lettersSeparated.length - 1];
+        lettersSeparated= lettersSeparated.slice(0,-1);
+        Reversed +=letters;
+        b++;
+    }
+    return Reversed;
+}
 // Do not edit below this line
 module.exports = reverseString;
