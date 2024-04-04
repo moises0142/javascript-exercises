0b953d47124f24175e722211c12f0e0120b956d3
moises0142 <lopez.moises0142@gmail.com>
2024-04-03 21:18:14 -0400

    did exercise 5 gave up on 4 after like 2 hour

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let i=+num1;
+    let newNumber=0;
+    if(num1<0 || num2<0 || (typeof num1 !== 'number') || (typeof num2 !== 'number')){
+        return 'ERROR';
+    }
+    else{
+    if(num1>num2){
+        i=num2;
+        while(num2<=i && i<=num1)
+        {
+            newNumber += i;
+            i++
+        }
+        return newNumber;
+    }
+    while(num1<=i && i<=num2)
+    {
+        newNumber += i;
+        i++
+    }
+    }   
+    return newNumber;
 };
 
 // Do not edit below this line
