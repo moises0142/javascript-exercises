49fb32a94357af61b703b38617f83e967f7fbb88
moises0142 <lopez.moises0142@gmail.com>
2024-04-03 22:39:02 -0400

    finished numero 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  
+
+  convertedTemp = (temp-32) * 5/9;
+  convertedTemp = Math.round(convertedTemp*10) / 10;
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = temp * (9/5)+ 32;
+  convertedTemp = Math.round(convertedTemp*10) / 10;
+  return convertedTemp;
 };
 
 // Do not edit below this line
