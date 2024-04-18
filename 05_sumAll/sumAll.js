7a4818673f906111dd7cf029bb946bf5e9ea2e70
moises0142 <lopez.moises0142@gmail.com>
2024-04-18 16:57:30 -0400

    passed all exercises in 08 calculator

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(num1, num2) {
-    let i=+num1;
+    let i=num1;
     let newNumber=0;
     if(num1<0 || num2<0 || (typeof num1 !== 'number') || (typeof num2 !== 'number')){
         return 'ERROR';
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(num1,num2) {
+  let sum = 0;
+  sum = num1+num2;
+  return sum;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+
+  let result = 0;
+  result = num1-num2;
+  return result;
+};
+
+const sum = function(num1) {
+	  let sum = 0; 
+
+    let numeral1 = num1.reduce((total,currentItem)=> total +currentItem,0);
+
+    sum=numeral1;
+
+    return sum;
+};
+
+const multiply = function(num1) {
+
+  let result = num1.reduce((total,currentItem)=>total * currentItem,1);
+
+  return result;
+};
+
+const power = function(num1,num2) {
 	
+  let result = Math.pow(num1,num2);
+  return result;
+  
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num1) {
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  let array = [...new Array(num1)].map((num,i) => i+1);
+  let result = array.reduce((total,currentItem)=>total * currentItem,1);
+  return result;
 	
 };
 
