199996d24ca8c94301a7a32a1b626844d4a7730d
moises0142 <lopez.moises0142@gmail.com>
2024-04-20 03:24:19 -0400

    did exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let array = [0,1];
+    let error = 'OOPS';
+    if(num>=0){
+        for(let i=0; i<=num; i++){
+            let finoNum =array[array.length-1]+array[i];
+            array.push(finoNum);
+        }
+        array.pop();
+        array.pop();
+        return array[num];
+    }
+    else{
+        return error;
+    }
 };
 
 // Do not edit below this line
