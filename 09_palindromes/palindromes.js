96bc1be621d848856dcb69097a837192493e0543
moises0142 <lopez.moises0142@gmail.com>
2024-04-20 02:11:14 -0400

    finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+
+    let sentenceNoPunctuation = removePunctuation(sentence) 
+    let splitIntoArray = sentenceNoPunctuation.split('');
+    let reverseArray = splitIntoArray.reverse();
+    let reverseJoinedArray = reverseArray.join('');
+    
+    function removePunctuation(text){
+        let punctuation = ['.',',','!',' '];
+        let textLowerCase = text.toLowerCase();
+        let result = '';
+        for(let i = 0; i < text.length; i++){
+            if(!punctuation.includes(text[i])){
+                result += textLowerCase[i];
+            }
+        }
+        return result;
+    }
+    if (reverseJoinedArray === sentenceNoPunctuation){
+        return true;
+    }
+    else{
+        return false;
+    }
 
 };
 
