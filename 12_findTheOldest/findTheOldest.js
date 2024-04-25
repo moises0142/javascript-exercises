5eab79a56ecfc88723a958960b2ab89232766d95
moises0142 <lopez.moises0142@gmail.com>
2024-04-25 02:40:04 -0400

    did exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(years) {
 
-};
+    let currentYear = new Date().getFullYear();
+    console.log(currentYear);
+    years.map(person=> {
+        if(person.yearOfDeath == undefined){
+            person.yearOfDeath = currentYear;
+            return person;
+        }     
+    });
+
+    peopleSorted =years.sort((a,b)=>((a.yearOfDeath- a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)? -1:1));
+    oldest = peopleSorted[0];
+    return oldest;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
